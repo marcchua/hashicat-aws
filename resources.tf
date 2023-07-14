93f3de6aaf69e1f5ef01bf476aeec9f9f3492081
marcchua <marc.ceasar@gmail.com>
2023-07-14 06:03:04 -0400

    added kube for demo

---
+++ resources.tf
@@ -0,0 +1,40 @@
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = "demo-morgan-infracost"
+  cluster_version = "1.27"
+
+  vpc_id                         = aws_vpc.hashicat.id
+  # subnet_ids                     = module.vpc.private_subnets
+  subnet_ids                     = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
